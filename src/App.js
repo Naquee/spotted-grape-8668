372747c52390cb050f7750f22f3fbc077e72d5b4
Naquee <100348982+Naquee@users.noreply.github.com>
2022-08-26 00:31:55 +0530

    update day2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 import { Home } from './Components/Home/Home';
 import {Routes , Route} from "react-router-dom"
 import { Cart } from './Components/pages/Cart';
+import { Class } from './Components/Home/Class';
+import { Navbar } from './Components/Navbar/Navbar';
+import { NavBottom } from './Components/Navbar/NavBottom';
+import { NavAllPages } from './Components/Navbar/NavAllPages';
+import { BigImage } from './Components/DivImage/BigImage';
+import { QuickBuy } from './Components/Body/QuickBuy';
 
+import { ModalFooter } from '@chakra-ui/react';
+import Footer from './Components/Footer/Footer';
 import { LogIn } from './Components/pages/LogIn';
 import { SuperShop } from './Components/pages/SuperShop';
 
---
+++ src/Components/pages/SuperShop.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SuperShop = () => {
+  return (
+    <div>SuperShop</div>
+  )
+}
+
+export default SuperShop
